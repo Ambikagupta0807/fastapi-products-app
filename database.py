d7cb4eccaa730e6909bae95a97fc2c3f246c3de4
Ambika <ambikagupta8899@gmail.com>
2026-09-19 01:48:56 +0530

    fixed database URL

--- database.py
+++ database.py
@@ -3,5 +3,9 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
 db_url = os.getenv("DATABASE_URL", "mysql+pymysql://root:NewPassword%40123@localhost/myproject")
+
+if db_url.startswith("mysql://"):
+    db_url = db_url.replace("mysql://", "mysql+pymysql://", 1)
+
 engine = create_engine(db_url)
 session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
